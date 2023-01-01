fcee110ada419cbe26cb6fc340757926c4d1c402
KaisarV <kaisar.valentino123@gmail.com>
2023-01-01 22:51:42 +0700

    Initial commit

---
+++ app/src/main/java/com/calc/belajar_dev_android/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.calc.belajar_dev_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var  nameEditText: EditText
+    private lateinit var  sayHelloButton: Button
+    private lateinit var  sayHelloTextView: TextView
+
+    private fun initComponents(){
+        nameEditText = findViewById(R.id.nameEditText)
+        sayHelloButton = findViewById(R.id.sayHelloButton)
+        sayHelloTextView = findViewById(R.id.sayHelloTextView)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.hello_world)
+
+        initComponents()
+
+        sayHelloTextView.text = resources.getString(R.string.app_name)
+
+        sayHelloButton.setOnClickListener{
+//            Log.d("PGZN", "This is debug log")
+//            Log.i("PGZN", "This is info log")
+//            Log.w("PGZN", "This is warning log")
+//            Log.e("PGZN", "This is error log")
+
+            Log.i("ValueResource", resources.getBoolean(R.bool.isProductionMode).toString())
+            Log.i("ValueResource", resources.getInteger(R.integer.maxPage).toString())
+            Log.i("ValueResource", resources.getIntArray(R.array.numbers).toString())
+            Log.i("ValueResource", resources.getColor(R.color.background, theme).toString())
+
+            val name = nameEditText.text.toString()
+            sayHelloTextView.text = resources.getString(R.string.sayHelloTextView, name)
+
+            sayHelloButton.setBackgroundColor(resources.getColor(R.color.background, theme))
+
+//            resources.getStringArray(R.array.names).forEach{
+//                Log.i("StringArray", it.toString())
+//            }
+        }
+
+    }
+}
